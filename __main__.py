d4a0aed8b88570334cf382b59b757564bd6de536
Amelia2504 <115760437+Amelia2504@users.noreply.github.com>
2022-10-14 03:25:29 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,13 +1,13 @@
 import movies_dataset as movies
 import movies_genre_model
 
-min_year = 1977
-max_year = 2017
-epochs = 50
+min_year = 1990
+max_year = 2000
+epochs = 5
 genres = movies.list_genres(7)
 
 # select a smaller ratio (e.g. 40) for quicker training
-for ratio in [60]:
+for ratio in [40]:
     # we load the data once for each ratio, so we can use it for multiple versions, epochs, etc.
     x_train, y_train = movies.load_genre_data(min_year, max_year, genres, ratio, 'train')
     x_validation, y_validation = movies.load_genre_data(min_year, max_year, genres, ratio, 'validation')
